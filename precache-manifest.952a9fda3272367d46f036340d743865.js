0a3358b3949bb117274e52a10fde0c462379dd00
Mike1999L9 <68628634+Mike1999L9@users.noreply.github.com>
2021-08-24 18:04:55 +1000

    Updates

---
+++ precache-manifest.952a9fda3272367d46f036340d743865.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edde085600b9a7327d86",
+    "url": "/SimpleReactPortfolio/static/css/main.4459f9ae.chunk.css"
+  },
+  {
+    "revision": "edde085600b9a7327d86",
+    "url": "/SimpleReactPortfolio/static/js/main.edde0856.chunk.js"
+  },
+  {
+    "revision": "057fcbe0f0a0891a2603",
+    "url": "/SimpleReactPortfolio/static/js/runtime~main.057fcbe0.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/SimpleReactPortfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "1b14e4d3a1b02bea6d60ddda636826af",
+    "url": "/SimpleReactPortfolio/index.html"
+  }
+];
