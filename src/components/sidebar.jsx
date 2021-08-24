2e73ec56fb649fb87efbff40f9801ed74cfc860d
Mike1999L9 <68628634+Mike1999L9@users.noreply.github.com>
2021-08-24 18:32:50 +1000

    Update sidebar.jsx

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/AboutMe.JPG)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/AboutMe.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">Jerry Ho</a></h1>
               <span className="email"><i className="icon-mail"></i> jerrysiuhin@gmail.com</span>
             </div>
